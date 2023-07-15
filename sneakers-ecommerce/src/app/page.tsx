9b51ffabe4b5d42865c47b45f6e89e1c543b5d1f
RedDotz20 <carlos_tabangay@outlook.com>
2023-07-16 01:18:28 +0800

    feat: sneakers add product state

--- sneakers-ecommerce/src/app/page.tsx
+++ sneakers-ecommerce/src/app/page.tsx
@@ -1,25 +1,34 @@
+import AddProduct from '@/components/AddProduct';
+
 export default function Home() {
 	return (
 		<main className="h-screen">
 			<section className="m-10">
-				<h3 className="text-[#DA833E] font-semibold">SNEAKER COMPANY</h3>
-				<h1 className="font-bold">Fall Limited Edition Sneakers</h1>
-				<p className="text-[#5C5B5F]">
-					These low-profile sneakers are your perfect casual wear comparison.
-					Featuring a durable rubber outer sole, they'll withstand everything
-					the weather can offer
-				</p>
+				<div>
+					<h3 className="text-[#DA833E] font-semibold mb-1 text-xs">
+						SNEAKER COMPANY
+					</h3>
+					<h1 className="mb-4 text-3xl font-bold">
+						Fall Limited Edition Sneakers
+					</h1>
+					<p className="text-[#5C5B5F]">
+						These low-profile sneakers are your perfect casual wear comparison.
+						Featuring a durable rubber outer sole, they'll withstand everything
+						the weather can offer
+					</p>
 
-				<p className="flex items-center">
-					$125.00
-					<span className="text-[#DA833E] font-bold bg-[#FEEFE2] px-1 text-[10px] rounded-sm inline-flex pl-2">
-						50%
-					</span>
-				</p>
+					<p className="flex items-center text-xl font-black">
+						$125.00
+						<span className="text-[#DA833E] font-bold bg-[#FEEFE2] ml-4 text-[0.6em] rounded-lg inline-flex px-2">
+							50%
+						</span>
+					</p>
 
-				<span>$250.00</span>
+					<span className="text-[#5C5B5F] line-through text-xs">$250.00</span>
+
+					<AddProduct />
+				</div>
 			</section>
 		</main>
-		// bg-[#FEEFE2]
 	);
 }
--- sneakers-ecommerce/src/components/AddProduct.tsx
+++ sneakers-ecommerce/src/components/AddProduct.tsx
@@ -1,14 +1,54 @@
 'use client';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function AddProduct() {
+	const [quantity, setQuantity] = useState(0);
+
+	const increaseQuantity = () => {
+		if (quantity < 10) setQuantity((prev) => prev + 1);
+	};
+
+	const decreaseQuantity = () => {
+		if (quantity > 0) setQuantity((prev) => prev - 1);
+	};
+
 	return (
-		<>
-			<div>
-				<button></button>
-				<p>{}</p>
-				<button></button>
+		<div className="flex gap-4">
+			<div className="bg-[#F7F8FD] flex gap-2 items-center">
+				<button
+					className="h-full px-5 cursor-pointer"
+					onClick={decreaseQuantity}
+				>
+					<Image
+						src="icon-minus.svg"
+						alt="cart"
+						width={10}
+						height={10}
+					/>
+				</button>
+				<p className="w-10 px-2 text-center">{quantity}</p>
+				<button
+					className="h-full px-5 cursor-pointer"
+					onClick={increaseQuantity}
+				>
+					<Image
+						src="icon-plus.svg"
+						alt="cart"
+						width={10}
+						height={10}
+					/>
+				</button>
 			</div>
-			<button>Add to cart</button>
-		</>
+			<button className="bg-[#FF7D1B] text-white py-4 px-20 rounded-md text-[1rem] font-semibold flex gap-4">
+				<Image
+					src="icon-cart.svg"
+					alt="cart"
+					width={22}
+					height={20}
+				/>
+				Add to cart
+			</button>
+		</div>
 	);
 }
